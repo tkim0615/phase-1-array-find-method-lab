5f9c8115ae01ff42ebe8e51df0b3626a892cc962
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-19 11:16:02 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record){
+    const winningYear = record.find(element => element.result === 'W')
+    if (winningYear) {
+    return winningYear.year
+    }
+
+    }
